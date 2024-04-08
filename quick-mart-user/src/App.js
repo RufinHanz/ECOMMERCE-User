09332ac3c34ba8d75a59f96ee4d7e665bb68fa04
RufinHanz <21-1-00703@gmail.com>
2024-04-08 05:18:21 -0800

    finished

--- quick-mart-user/src/App.js
+++ quick-mart-user/src/App.js
@@ -18,6 +18,7 @@ const App = () => {
     return (
         <Router>
             <Routes>
+            <Route path="/" element={<Login />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/shop" element={<Shop cart={cart} setCart={setCart} />} />
